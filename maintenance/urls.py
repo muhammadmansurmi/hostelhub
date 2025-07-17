da09092963e81023c865ba82c3e7277e2645b4d2
MIAGARO <muhammadmansurisahgaro@gmail.com>
2025-07-16 18:18:54 -0700

    Initial commit

--- maintenance/urls.py
+++ maintenance/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import submit_request, view_requests
 
 urlpatterns = [
-    path('', submit_request, name='submit'),         # http://127.0.0.1:8000/submit/
-    path('all/', view_requests, name='view_requests')  # http://127.0.0.1:8000/submit/all/
+    path('', submit_request, name='submit'),
+    path('all/', view_requests, name='view_requests'),
 ]
